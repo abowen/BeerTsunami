ce3bce8f9496854b55a2f46168149aabf1e8407c
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2017-01-21 13:22:30 +1100

    water level sim

---
+++ Assets/Scripts/WaterLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WaterLevel : MonoBehaviour {
+
+    private int frame;
+    private float yMax;
+
+    public float aCoeff { get; set; }
+    public float bCoeff { get; set; }
+    public float fCoeff { get; set; }
+
+    void Start () {
+        frame = 0;
+        aCoeff = 0.001f;
+        bCoeff = 0.3f;
+        fCoeff = 0.01f;
+        yMax = 5.5f;
+	}
+
+    private void Update()
+    {
+        frame++;
+
+        var currentVector = transform.position;
+
+        transform.position = new Vector3
+        {
+            x = currentVector.x,
+            z = currentVector.z,
+            y = currentVector.y < yMax ? aCoeff * frame + bCoeff* Mathf.Sin(fCoeff * frame) : currentVector.y
+        };
+    }
+}
