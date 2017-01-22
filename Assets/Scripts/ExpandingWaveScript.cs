90bccdc90cdf399930f16a4cb5152feb90dd2ed4
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-01-22 13:38:47 +1100

    Added wave to playground

---
+++ Assets/Scripts/ExpandingWaveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandingWaveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localScale += new Vector3(0.02f, 0, 0);
+        gameObject.transform.position += new Vector3(0.01f, 0, 0);
+	}
+}
