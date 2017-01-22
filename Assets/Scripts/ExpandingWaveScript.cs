0b5169ee98dccb89d2d4a755b7cfcef2d48a72e5
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-01-22 14:39:58 +1100

    PAY TO WIN!

--- Assets/Scripts/ExpandingWaveScript.cs
+++ Assets/Scripts/ExpandingWaveScript.cs
@@ -13,7 +13,7 @@ public class ExpandingWaveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {        
-        gameObject.transform.localScale += new Vector3(0.02f * Speed, 0, 0);
+        gameObject.transform.localScale += new Vector3(0.04f * Speed, 0, 0);
         gameObject.transform.position += new Vector3(0.01f * Speed, 0, 0);
         Camera.main.transform.position += new Vector3(0.005f * Speed, 0, 0);
     }
