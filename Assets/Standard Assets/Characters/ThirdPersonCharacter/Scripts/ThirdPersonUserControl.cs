8eb7a8b64460ce5483ed23bef8deddfefca26834
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2017-01-22 12:28:32 +1100

    2 player support

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -4,14 +4,16 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (ThirdPersonCharacter))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
+        public int PlayerNumber = 1;
+
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
         private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
-            
+
         private void Start()
         {
             // get the transform of the main camera
@@ -34,25 +36,40 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+
+            float h = 0;
+            float v = 0;
+            bool crouch = false;
+
+            if (PlayerNumber == 1)
+            {
+                h = CrossPlatformInputManager.GetAxis("P1_Horizontal");
+                v = CrossPlatformInputManager.GetAxis("P1_Vertical");
+                crouch = Input.GetKey(KeyCode.C);
+            }
+            else if (PlayerNumber == 2)
+            {
+                h = CrossPlatformInputManager.GetAxis("P2_Horizontal");
+                v = CrossPlatformInputManager.GetAxis("P2_Vertical");
+                crouch = Input.GetKey(KeyCode.Keypad0);
+            }
+
 
             // calculate move direction to pass to character
             if (m_Cam != null)
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+                m_Move = v * m_CamForward + h * m_Cam.right;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                m_Move = v * Vector3.forward + h * Vector3.right;
             }
 #if !MOBILE_INPUT
-			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+            // walk speed multiplier
+            if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
 
             // pass all parameters to the character control script
